d352f4916f65872f0d924326c02e872d5c6e4c61
Joshua Fern <joshuafern@protonmail.com>
2023-06-26 12:41:50 -0700

    fdroid update

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 fdroid update --rename-apks --create-metadata --pretty
-git add .
-git commit -m "fdroid update"
+
+## I'm having an issue with fdroidserver right now so the auto-commit is probably a bad idea.
+#git add .
+#git commit -m "fdroid update"
